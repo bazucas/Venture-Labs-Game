07791599e3900b0d474a1a92373c3d4cfb59a8a3
Luís Inácio <lrs.inacio@gmail.com>
2023-08-26 01:14:35 +0100

    Weather controller test - remove after

--- src/API/VL.Taskio.TaskService/Controllers/WeatherForecastController.cs
+++ src/API/VL.Taskio.TaskService/Controllers/WeatherForecastController.cs
@@ -6,8 +6,7 @@ namespace VL.Taskio.TaskService.Controllers;
 [Route("api/[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
-    {
+    private static readonly string[] _summaries = {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
@@ -25,7 +24,7 @@ public class WeatherForecastController : ControllerBase
         {
             Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
             TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            Summary = _summaries[Random.Shared.Next(_summaries.Length)]
         })
         .ToArray();
     }
